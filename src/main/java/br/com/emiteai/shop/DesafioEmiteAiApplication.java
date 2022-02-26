f7c68fd5e7d17c89f1dcad90fab74da161913ccf
Alanis2 <alanisemanuela950@gmail.com>
2022-02-26 01:34:02 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/emiteai/shop/DesafioEmiteAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emiteai.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEmiteAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioEmiteAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/emiteai/shop/DesafioEmiteAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emiteai.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEmiteAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
